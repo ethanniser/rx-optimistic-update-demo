1d6d5853fe6fdb12a1d4cbaa165e0c246d878d2a
Ethan Niser <ethan@niser.com>
2025-07-30 11:30:51 -0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
-import { useRx, useRxValue } from "@effect-rx/rx-react";
+import { useRx, useRxRefresh, useRxSet, useRxValue } from "@effect-rx/rx-react";
 import {
   updateFailsRx,
   addTodoRx,
   removeTodoRx,
+  optimisticAddTodosRx,
+  optimisticRemoveTodosRx,
+  todosRx,
   optimisticTodosRx,
 } from "./rx";
 import { useState } from "react";
 
 export default function App() {
   const [updateFails, setUpdateFails] = useRx(updateFailsRx);
-  const todos = useRxValue(optimisticTodosRx);
-  const [addTodoState, addTodo] = useRx(addTodoRx);
-
   const [input, setInput] = useState("");
+  const trueTodos = useRxValue(todosRx);
+  const optimisticTodos = useRxValue(optimisticTodosRx);
+  const addTodo = useRxSet(optimisticAddTodosRx);
+  const addTodoState = useRxValue(addTodoRx);
+
+  const manuallyRefresh = useRxRefresh(todosRx);
+
   return (
     <div className="p-4 max-w-lg mx-auto space-y-4">
       <div className="">
         <p className="text-lg mb-2">Will fail: {updateFails.toString()}</p>
         <button
+          type="button"
           onClick={() => setUpdateFails(!updateFails)}
           className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
         >
           Toggle update fails
         </button>
       </div>
+      <button
+        type="button"
+        onClick={() => manuallyRefresh()}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+      >
+        Manually refresh
+      </button>
       <pre>{JSON.stringify(addTodoState, null, 2)}</pre>
 
       <div className="flex gap-2">
@@ -34,33 +49,46 @@ export default function App() {
           className="flex-1 border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button
+          type="button"
           onClick={() => addTodo(input)}
-          disabled={addTodoState.waiting}
+          // disabled={addTodoState.waiting}
           className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded disabled:opacity-50"
         >
           {addTodoState.waiting ? "Adding..." : "Add"}
         </button>
       </div>
 
+      <h1>Optimistic Todos</h1>
       <div className="space-y-4">
-        {todos.map((todo) => (
-          <TodoItem key={todo} todo={todo} />
+        {optimisticTodos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </div>
+
+      <h1>True Todos</h1>
+      <div className="space-y-4">
+        {trueTodos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
         ))}
       </div>
     </div>
   );
 }
 
-function TodoItem({ todo }: { todo: string }) {
-  const [removeTodoState, removeTodo] = useRx(removeTodoRx(todo));
+function TodoItem({ todo }: { todo: { id: number; text: string } }) {
+  const removeTodo = useRxSet(optimisticRemoveTodosRx);
+  const removeTodoState = useRxValue(removeTodoRx);
   return (
     <div
-      key={todo}
+      key={todo.id}
       className="flex items-center justify-between p-4 bg-gray-100 rounded"
     >
-      <p className="text-gray-800">{todo}</p>
+      <p className="text-gray-800">
+        {todo.id}: {todo.text}
+      </p>
       <button
-        onClick={() => removeTodo()}
+        type="button"
+        onClick={() => removeTodo(todo.id)}
         disabled={removeTodoState.waiting}
         className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-3 rounded disabled:opacity-50"
       >
--- src/rx.ts
+++ src/rx.ts
@@ -3,38 +3,58 @@ import { Rx } from "@effect-rx/rx-react";
 
 export const updateFailsRx = Rx.make(false);
 
-export const todosRx = Rx.make<string[]>([]);
-
-export const optimisticTodosRx = Rx.writable(
-  (get) => get(todosRx),
-  (ctx, value: string[]) => ctx.setSelf(value)
-);
+let id = 0;
+let todos: {
+  id: number;
+  text: string;
+}[] = [];
+export const todosRx = Rx.make<{ id: number; text: string }[]>(() => {
+  console.log("todosRx", todos);
+  return todos;
+});
+export const optimisticTodosRx = Rx.optimistic(todosRx);
 
 export const addTodoRx = Rx.fn(
-  Effect.fnUntraced(function* (todo: string) {
-    const todos = yield* Rx.get(todosRx);
-    yield* Rx.set(optimisticTodosRx, [...todos, todo]);
-    yield* Effect.addFinalizer(() => Rx.refresh(optimisticTodosRx));
+  Effect.fnUntraced(function* (todo: string, get: Rx.FnContext) {
+    console.log("addTodoRx", todo);
     yield* Effect.sleep("1 second");
-    if (yield* Rx.get(updateFailsRx)) {
+    if (get(updateFailsRx)) {
       yield* Effect.fail("Update failed");
     }
-    yield* Rx.set(todosRx, [...todos, todo]);
-  }, Effect.scoped)
+
+    todos.push({ id: id++, text: todo });
+  })
 );
 
-export const removeTodoRx = Rx.family((todo: string) =>
-  Rx.fn(
-    Effect.fnUntraced(function* () {
-      const todos = yield* Rx.get(todosRx);
-      yield* Effect.sleep("1 second");
-      if (yield* Rx.get(updateFailsRx)) {
-        yield* Effect.fail("Update failed");
-      }
-      yield* Rx.set(
-        todosRx,
-        todos.filter((t) => t !== todo)
-      );
-    })
-  )
+export const optimisticAddTodosRx = optimisticTodosRx.pipe(
+  Rx.optimisticFn({
+    updateToValue: (todo: string, current: { id: number; text: string }[]) => {
+      console.log("optimisticAddTodosRx", todo);
+      return [...current, { id: id++, text: todo }];
+    },
+    fn: addTodoRx,
+  })
+);
+
+export const removeTodoRx = Rx.fn(
+  Effect.fnUntraced(function* (id: number, get: Rx.FnContext) {
+    console.log("removeTodoRx", id);
+    yield* Effect.sleep("1 second");
+    if (get(updateFailsRx)) {
+      yield* Effect.fail("Update failed");
+    }
+    console.log("before", todos);
+    todos = todos.filter((t) => t.id !== id);
+    console.log("after", todos);
+  })
+);
+
+export const optimisticRemoveTodosRx = optimisticTodosRx.pipe(
+  Rx.optimisticFn({
+    updateToValue: (id: number, current: { id: number; text: string }[]) => {
+      console.log("optimisticRemoveTodosRx", id);
+      return current.filter((t) => t.id !== id);
+    },
+    fn: removeTodoRx,
+  })
 );
